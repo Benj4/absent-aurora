3411547217c706e989bee55c8a88019c229f04ba
B Diaz <bdiazcaballero@gmail.com>
2025-12-31 00:55:50 -0300

    feat: add monthly series data page with form for data submission
    - Created a new Astro page for adding monthly series data.
    - Implemented static paths generation based on monthly frequency indicators.
    - Added a form for users to input monthly values, data source, and date.

---
+++ src/types/jsx.d.ts
@@ -0,0 +1,12 @@
+export {};
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      key?: string | number;
+    }
+
+    interface HTMLAttributes<T = any> {
+      key?: string | number;
+    }
+  }
+}
